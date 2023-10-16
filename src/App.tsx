7df67e7c6c2dd48738107d132ea8ce3367eff0a1
Raphael Brito <rcb@cesar.org.br>
2023-10-16 15:24:34 -0300

    Primeira parte da aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Todo } from "./types/Todo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = () =>
+    fetch("http://localhost:5000/todos")
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+
+  const handleCreateTodo = () => {
+    fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: newTodo, completed: false }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos((oldTodos) => [...oldTodos, data]);
+        setNewTodo("");
+      });
+  };
+
+  const handleUpdateTodo = (todo: Todo) => {
+    fetch(`http://localhost:5000/todos/${todo.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...todo, completed: !todo.completed }),
+    }).then(() => getTodos());
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => getTodos());
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="Nova Tarefa"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleCreateTodo}>Criar nova tarefa</button>
+      <h1>Lista de Tarefas:</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <h3>{todo.text}</h3>
+            <button onClick={() => handleUpdateTodo(todo)}>
+              {todo.completed ? "Recomeçar" : "Finalizar"}
+            </button>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Excluir</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
