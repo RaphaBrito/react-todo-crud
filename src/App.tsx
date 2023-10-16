b850ffafee6fe7502f9be578c68a512e25167818
Raphael Brito <rcb@cesar.org.br>
2023-10-16 10:58:50 -0300

    Adicionando novo item e organizando botões

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Todo } from "./types/Todo";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/todos")
@@ -11,14 +12,39 @@ function App() {
       .then((data) => setTodos(data));
   }, []);
 
+  const handleCreateTodo = () => {
+    fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: newTodo, completed: false }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos([...todos, data]);
+        setNewTodo("");
+      });
+  };
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Nova tarefa"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleCreateTodo}>Adicionar Tarefa</button>
       <h1>Lista de Tarefas</h1>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
             <h3>{todo.text}</h3>
-            <button>{todo.completed ? "Recomeçar" : "Concluir"}</button>
+            <div className="item-buttons">
+              <button>{todo.completed ? "Recomeçar" : "Concluir"}</button>
+              <button>Excluir</button>
+            </div>
           </li>
         ))}
       </ul>
